6f6ef61c95f993a184fe032f4a96bb5b4a9f3577
David Pietroniro <david@pietroniro.com.br>
2018-07-31 15:47:17 -0300

    Proxy Hands-on

---
+++ SegurancaProxy/test/TesteProxySeguranca.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TesteProxySeguranca {
+
+	@Test
+	public void testeAutorizaAcesso() {
+		Usuario guerra = new Usuario("Guerra");
+		
+		guerra.autorizaAcesso("InterfaceNegocio", "executaTransacao");
+		NegocioMock mock = new NegocioMock();
+		InterfaceNegocio n = new SegurancaNegocio(mock, guerra);
+		n.executaTransacao();
+		assertTrue(mock.foiAcessado());
+	}
+
+	@Test
+	public void testeNaoAutorizaAcesso() {
+		Usuario guerra = new Usuario("Guerra");
+		
+		guerra.autorizaAcesso("InterfaceNegocio", "executaTransacao");
+		NegocioMock mock = new NegocioMock();
+		InterfaceNegocio n = new SegurancaNegocio(mock, guerra);
+		try {
+			n.cancelaTransacao();
+			fail();
+		} catch (Exception e) {
+			assertFalse(mock.foiAcessado());
+		}
+	}
+
+}
